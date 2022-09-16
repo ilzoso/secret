7271700d899df4e584bc106589b36895c6a104b2
ilzoso <ilzoso@yahoo.com>
2022-09-15 22:51:21 -0700

    reading resource

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using secret;
+using System.Reflection;
+
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine(args.Length > 0 ? args[0] : "nothing to see here...");
 Console.WriteLine("... " + Environment.GetEnvironmentVariable("KEY"));
+
+string[] resNames = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+foreach (string resName in resNames)
+{
+    Console.WriteLine($"Resource = {resName}");
+    var rc = ResourceUtil.GetEmbeddedResourceContent(resName);
+    Console.WriteLine($"{rc}");
+}
 
---
+++ ResourceUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secret
+{
+    internal class ResourceUtil
+    {
+
+        public static string GetEmbeddedResourceContent(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            using (Stream stream = asm.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader source = new StreamReader(stream))
+                {
+                    string fileContent = source.ReadToEnd();
+                    return fileContent;
+                }
+            }
+        }
+    }
+}
